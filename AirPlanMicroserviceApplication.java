e616f6a22fe8c458c7169b869717df304770b438
AsunaMYAdmin <kiritoadm@mail.ru>
2025-03-01 23:29:40 +0700

    Init Commit

---
+++ AirPlanMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package me.asunamyadmin.air_plan_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPlanMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirPlanMicroserviceApplication.class, args);
+    }
+
+}
